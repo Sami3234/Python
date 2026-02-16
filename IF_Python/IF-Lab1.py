08b2e17907fff186dd3c826478b68f12d0365ea3
Muhammad Sami Ullah <kk34samikhan@gmail.com>
2026-02-16 10:13:50 +0500

    update folder wise Ai and IF

---
+++ IF_Python/IF-Lab1.py
@@ -0,0 +1,58 @@
+# #Avarage
+# s1=int (input("Enter your first subject marks:"))
+# s2= int (input("Enter your 2nd subject marks:"))
+# s3= int (input("Enter your 3nd subject marks:"))
+# s4= int (input("Enter your 4nd subject marks:"))
+# s5= int (input("Enter your 5nd subject marks:"))
+# avg = (s1+s2+s3+s4+s5)/5
+# print (avg)
+
+
+#even and odd
+
+# num = int (input("Enter a number for check eveen and odd :"))
+# if (num%2== 0):
+#     print ("Even")
+# else:
+#     print ("odd")
+
+#number is prime or not
+
+# ...existing code...
+
+# ...existing code...
+
+# num = int(input("Enter a number for check prime or not: "))
+
+# if num < 2:
+#     print("Not prime")
+# else:
+#     i = 2
+#     flag = 1
+#     while i * i <= num:
+#         if num % i == 0:
+#             flag = 0
+#             break
+#         i += 1
+
+#     if flag == 1:
+#         print("Prime number")
+#     else:
+#         print("Not prime")
+
+# calculate sum 10 numbers which user enter
+
+# sum_numbers.py
+total = 0
+count = 0
+while count < 10:
+    num = int(input("Enter a number: "))
+    total += num
+    count += 1
+print("Sum:", total)
+
+
+
+
+
+
